3056d0044fd33d873903ef42e0ffea0b59bf3981
MustapheMS <moztaphem9@gmail.com>
2023-06-25 22:57:34 +0300

    Done Dice Game

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-//declaring variables 
+// declaring variables
 const player1 = document.querySelector(".player--0");
 const player2 = document.querySelector(".player--1");
 const score1 = document.querySelector("#score--0");
@@ -8,78 +7,78 @@ const current1 = document.querySelector("#current--0");
 const current2 = document.querySelector("#current--1");
 const ludo = document.querySelector(".dice");
 
-//main variables 
+// main variables
 let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
-score1.innerText = 0
-score2.innerText = 0
-current1.innerText = 0
+let playing = false;
+
+score1.innerText = 0;
+score2.innerText = 0;
+current1.innerText = 0;
 current2.innerText = 0;
 
-
-//switchPlayerr function
+// switchPlayer function
 function switchP() {
-    currentScore = 0;
-    if (activePlayer === 0) {
-        activePlayer = 1;
-    } else {
-        activePlayer = 0
-    }
-    player1.classList.toggle("player--active");
-    player2.classList.toggle("player--active");
+  currentScore = 0;
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  player1.classList.toggle("player--active");
+  player2.classList.toggle("player--active");
 }
 
-
-//button roll after click action
+// button roll after click action
 document.querySelector(".btn--roll").addEventListener("click", function () {
-    //playing is true
-    playing = true
-    //generating random number
-    const dice = Math.floor(Math.random() * 6) + 1
-    ludo.src = "dice-" + dice + (".png")
-    //checking if is not an conditions
-    if (dice != 1) {
-        //to display the images score into score as number
-        currentScore += dice
-        document.querySelector("#current--" + activePlayer).innerText = currentScore;
+  if (!playing) return;
+  
+  // generating random number
+  const dice = Math.floor(Math.random() * 6) + 1;
+  ludo.src = "dice-" + dice + ".png";
 
-    }
-    else {
-        switchP()
-    }
+  // checking if it's not a 1
+  if (dice !== 1) {
+    // to display the current score
+    currentScore += dice;
+    document.querySelector("#current--" + activePlayer).innerText = currentScore;
+  } else {
+    switchP();
+  }
+});
 
-})
-
-//button hold after click action
+// button hold after click action
 document.querySelector(".btn--hold").addEventListener("click", function () {
-    //playing is still true
-    playing = true
-    //storing currentScore into scores to display their addition
-    scores[activePlayer] += currentScore
-    document.querySelector("#score--" + activePlayer).innerText = scores[activePlayer];
-    //condition of if it is  100
-    if (scores[activePlayer] >= 100) {
-        ludo.classList.add("hidden");
-        document.querySelector(".player--" + activePlayer).classList.add("player--winner");
-        playing = false
-    }
-    else {
-        switchP()
-    }
+  if (!playing) return;
+  
+  // storing currentScore into scores
+  scores[activePlayer] += currentScore;
+  document.querySelector("#score--" + activePlayer).innerText = scores[activePlayer];
 
-})
+  // checking if the player won
+  if (scores[activePlayer] >= 100) {
+    ludo.classList.add("hidden");
+    document.querySelector(".player--" + activePlayer).classList.add("player--winner");
+    playing = false;
+  } else {
+    switchP();
+  }
+});
 
-
-
-//button newGame after click action
+// button newGame after click action
 document.querySelector(".btn--new").addEventListener("click", function () {
-    let scores = [0, 0];
-    let currentScore = 0;
-    let activePlayer = 0;
-    score1.innerText = 0
-    score2.innerText = 0
-    current1.innerText = 0
-    current2.innerText = 0;
-})
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
+  score1.innerText = 0;
+  score2.innerText = 0;
+  current1.innerText = 0;
+  current2.innerText = 0;
+
+  ludo.classList.remove("hidden");
+  player1.classList.remove("player--winner");
+  player2.classList.remove("player--winner");
+});
